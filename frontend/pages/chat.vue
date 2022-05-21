0229a87161ad3885213d8210fe2673dde817a2f8
smonist <s.wesp@gmx.net>
2022-05-21 16:15:01 +0200

    commit das zeug

--- frontend/pages/chat.vue
+++ frontend/pages/chat.vue
@@ -1,18 +1,3 @@
 <template>
-	<NuxtLayout>
-		<div class="grid grid-cols-2 place-items-center bg-hex-2b2921">
-			<button btn primary>Primary button</button>
-			<NuxtLink btn primary to="login">Already have an account?</NuxtLink>
-			<a btn secondary>Primary button</a>
-			<div>2</div>
-			<div>3</div>
-			<div>4</div>
-			<div>5</div>
-			<div>6</div>
-		</div>
-	</NuxtLayout>
+    <p>HENLO</p>
 </template>
-
-<script setup lang="ts"></script>
-
-<style lang="scss"></style>
