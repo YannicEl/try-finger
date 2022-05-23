cf97f15826436f10abb4d1bb741f09ab9843ad46
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2022-05-23 19:33:26 +0200

    pnpm workspace again?

--- backend/src/test/srcTest.ts
+++ backend/src/test/srcTest.ts
@@ -1,7 +1,10 @@
 import { logger, Response, Request } from 'firebase-functions';
+import { add } from 'lib';
 
 export const handler = async (req: Request, res: Response) => {
 	logger.log(req.body);
+
+	logger.log(add(1, 2));
 
 	try {
 		res.sendStatus(200);
