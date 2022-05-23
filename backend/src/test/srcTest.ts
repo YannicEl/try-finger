873cb169115f7f27cce3323bf5ecc8b517eb8671
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2022-05-23 21:06:09 +0200

    type test

--- backend/src/test/srcTest.ts
+++ backend/src/test/srcTest.ts
@@ -1,5 +1,5 @@
 import { logger, Response, Request } from 'firebase-functions';
-import { User, add } from 'lib';
+import { User } from 'lib';
 
 export const handler = async (req: Request, res: Response) => {
 	logger.log(req.body);
@@ -7,10 +7,6 @@ export const handler = async (req: Request, res: Response) => {
 	const user: User = { name: '1' };
 
 	logger.log(user);
-
-	const sum = add(1, 2);
-
-	logger.log(sum);
 
 	logger.log('lol');
 
