c74eea763abaa97bb471855cf432dfccffa1fa96
yannic <yannic.ellhotka@gmail.com>
2022-05-30 18:06:55 +0200

    onUpdate(() => console.log("yote"))

--- backend/src/functions/chat/index.ts
+++ backend/src/functions/chat/index.ts
@@ -1,8 +1,13 @@
 import { region } from 'firebase-functions';
 import { handler as onCreate } from './srcOnCreate.js';
+import { handler as onUpdate } from './srcOnUpdate.js';
 
 export const chat = {
 	onCreate: region('europe-west1')
 		.firestore.document('chats/{chatId}')
 		.onCreate(onCreate),
+
+	onUpdate: region('europe-west1')
+		.firestore.document('chats/{chatId}')
+		.onUpdate(onUpdate),
 };
---
+++ backend/src/functions/chat/srcOnUpdate.ts
@@ -0,0 +1,41 @@
+import { Chat } from '@try-finger/lib';
+import { QueryDocumentSnapshot } from 'firebase-admin/firestore';
+import { Change, EventContext, logger } from 'firebase-functions/v1';
+import { HttpsError } from 'firebase-functions/v1/auth';
+import { snapshotToData } from '../../helpers/firestore.js';
+import { updatedDiff } from 'deep-object-diff';
+import { useDbJoinedChats } from '../../composables/useDbJoinedChats.js';
+import { runTransaction } from '../../helpers/runTransaction.js';
+
+export const handler = async (
+	change: Change<QueryDocumentSnapshot>,
+	context: EventContext
+) => {
+	const before = snapshotToData<Chat>(change.before);
+	const after = snapshotToData<Chat>(change.after);
+	const updated = updatedDiff(before, after) as Partial<Chat>;
+
+	logger.info('before --------');
+	logger.info(before);
+
+	logger.info('after ---------');
+	logger.info(after);
+
+	logger.info('updated --------');
+	logger.info(updated);
+
+	try {
+		if (updated.name) {
+			await runTransaction(async (t) => {
+				after.members.forEach((userId) => {
+					const { updateT } = useDbJoinedChats(userId);
+
+					return updateT(after.id, { name: after.name }, t);
+				});
+			});
+		}
+	} catch (err) {
+		logger.error(err);
+		throw new HttpsError('internal', 'Internal Server Error');
+	}
+};
