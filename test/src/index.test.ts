70cc30fa6a7d0e08f0898903bc71c5f67750b7c4
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2022-06-05 19:50:53 +0200

    breh

--- test/src/index.test.ts
+++ test/src/index.test.ts
@@ -56,7 +56,7 @@ beforeEach(async (ctx) => {
 	ctx.messageId = 'messageId';
 
 	// clear DB before every test
-	// app.clearFirestore();
+	await app.clearFirestore();
 });
 
 describe('Collection: users/', () => {
@@ -210,8 +210,7 @@ describe('Collection: chats/', () => {
 			await setupTestData(ctx);
 
 			await assertSucceeds(getDoc(myDb, `chats/${myChatId}`));
-      console.log("2 ----------")
-			// await assertSucceeds(getDoc(myDb, `chats/${ourChatId}`));
+			await assertSucceeds(getDoc(myDb, `chats/${ourChatId}`));
 		});
 
 		it('cannot get chats where I am not a member', async (ctx) => {
